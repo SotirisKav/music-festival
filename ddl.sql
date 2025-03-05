30984aade1c9ebbe685c1df7e55356dfcc2fd2dd
sotiriskav <109356240+SotirisKav@users.noreply.github.com>
2025-03-05 17:51:13 +0200

    Add files via upload

---
+++ ddl.sql
@@ -0,0 +1,21 @@
+CREATE TABLE festival (
+    festival_year INT NOT NULL,
+    duration INT NOT NULL,
+    address VARCHAR(500) NOT NULL,
+    PRIMARY KEY (festival_year),
+    FOREIGN KEY (address) REFERENCES location(address)
+);
+
+DROP TABLE IF EXISTS location;
+CREATE TABLE location (
+	address VARCHAR(100) NOT NULL,
+	coordinates POINT NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    continent VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS event;
+CREATE TABLE event (
+	building VARCHAR(100) NOT NULL
+);
